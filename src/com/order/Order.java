f3de0e578b3666e6e3d0552bf43305f680470bb8
Jay <qkrwpgud1996@gmail.com>
2025-07-26 14:56:35 +0900

    20250726

---
+++ src/com/order/Order.java
@@ -0,0 +1,16 @@
+package com.order;
+
+import com.product.Product;
+import com.user.User;
+
+public class Order {
+
+    User user;
+    Product product;
+
+    public Order(User user, Product product) {
+        this.user = user;
+        this.product = product;
+    }
+
+}
---
+++ src/com/user/User.java
@@ -0,0 +1,7 @@
+package com.user;
+
+public class User {
+
+    String userId;
+    String name;
+}
